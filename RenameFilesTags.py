fc5c0cd70d7912d66dc62f087bcc3ea18d0b1ecf
Belley <Belleyy@users.noreply.github.com>
2021-01-05 09:53:29 +0100

    Update RenameFilesTags.py
    Select all scenes, no longer need tags and other things

--- RenameFilesTags.py
+++ RenameFilesTags.py
@@ -1,39 +1,15 @@
 import os
 import sqlite3
 import re
-import subprocess
+#import subprocess
 from pathlib import Path
-import time
 import progressbar
 
 # Your sqlite path
 dbpath = Path(r"C:\Users\Winter\.stash\Full.sqlite")
 print("Path:",dbpath)
 
-def gettingTagsID(name):
-    sqlite_select_Tags = "SELECT id from tags WHERE name = '" + name + "';"
-    cursor.execute(sqlite_select_Tags)
-    result = cursor.fetchone()
-    try:
-        id = result[0]
-        print(name+":", id)
-    except:
-        id = 'null'
-        print("Error while getting: " + name)
-    return str(id)
-
-def get_SceneID_fromTags(id):
-    sqlite_select_Query = "SELECT scene_id from scenes_tags WHERE tag_id = '" + id + "';"
-    cursor.execute(sqlite_select_Query)
-    record = cursor.fetchall()
-    print("[SceneTags](ID"+id+") Total Rows:",len(record))
-    array_ID=[]
-    for row in record:
-        array_ID.append(row[0])
-    list=",".join(map(str,array_ID))
-    return list
-
-def get_Perf_fromScene(id_scene):
+def get_Perf_fromSceneID(id_scene):
     perf_list=""
     cursor.execute("SELECT performer_id from performers_scenes WHERE scene_id =" + id_scene + ";")
     record = cursor.fetchall()
@@ -49,128 +25,125 @@ def get_Studio_fromID(id):
     cursor.execute("SELECT name from studios WHERE id =" + id + ";")
     record = cursor.fetchall()
     studio_name=str(record[0][0])
-    #print("Studio name: ",studio_name)
     return studio_name
 
 try:
     sqliteConnection = sqlite3.connect(dbpath)
     cursor = sqliteConnection.cursor()
-    print("Database successfully connected to SQLite")
-
-    # The name of your tags
-    id_tags = gettingTagsID('TAGS')
-    print("\n")
-
+    print("Database successfully connected to SQLite\n")
     edit=0
-    if (id_tags != "null"):
-        # Get all ID (scenes) with the tags
-        id_scene=get_SceneID_fromTags(id_tags)
-        sqlite_select_Query = "SELECT id,path,title,date,studio_id from scenes WHERE id in (" + id_scene + ");"
-        cursor.execute(sqlite_select_Query)
-        record = cursor.fetchall()
-        print("Total Rows: ", len(record))
-        progressbar_Index = 0
-        progress = progressbar.ProgressBar(redirect_stdout=True).start(len(record))
-        for row in record:
-            progress.update(progressbar_Index + 1)
-            progressbar_Index+=1
-            scene_ID=str(row[0])
-            scene_fullPath=str(row[1])
-            scene_Directory = os.path.dirname(scene_fullPath)
-            scene_Extension=os.path.splitext(scene_fullPath)[1]
-            scene_Title=str(row[2])
-            scene_Date=str(row[3])
-            scene_Studio_id=str(row[4])
+    sqlite_select_Query = "SELECT id,path,title,date,studio_id from scenes;"
+    cursor.execute(sqlite_select_Query)
+    record = cursor.fetchall()
+    print("Total Rows: ", len(record))
+    progressbar_Index = 0
+    progress = progressbar.ProgressBar(redirect_stdout=True).start(len(record))
+    for row in record:
+        progress.update(progressbar_Index + 1)
+        progressbar_Index+=1
+        scene_ID=str(row[0])
+        scene_fullPath=str(row[1])
+        scene_Directory = os.path.dirname(scene_fullPath)
+        scene_Extension=os.path.splitext(scene_fullPath)[1]
+        scene_Title=str(row[2])
+        scene_Date=str(row[3])
+        scene_Studio_id=str(row[4])
 
+        # By default, title contains extensions.
+        scene_Title = re.sub(scene_Extension + '$', '', scene_Title)
+
+        #  Look for duplicate title, if a other scene have same date and title it will skip it.
+        cursor.execute("SELECT path FROM scenes WHERE title='" + scene_Title.replace("'", "''") + "' AND date='" + scene_Date + "' AND NOT id='" + scene_ID + "';")
+        duplicateCheck = cursor.fetchall()
+        if (len(duplicateCheck) > 0):
+            problem=0
+            for dupl_row in duplicateCheck:
+                if (os.path.dirname(str(dupl_row[0])) == scene_Directory):
+                    with open('output.txt', 'a') as f:
+                        f.write('Duplicated title detected!\n')
+                        f.write('{} - {}\n'.format(scene_ID, scene_Title))
+                        f.write('{} - {}\n'.format(os.path.dirname(str(dupl_row[0])), scene_Directory))
+                    problem=1
+            if (problem == 1):
+                print("\n")
+                continue
+
+        performer_name=get_Perf_fromSceneID(scene_ID)
+        print("Performer name: ",performer_name)
+
+        studio_name=get_Studio_fromID(scene_Studio_id)
+        print("Studio name: ",studio_name)
+
+        # Date + Performer + Title + Studio (ex: 2016-12-29 Eva Lovia - Her Fantasy Ball [Sneaky Sex])
+        newfilename=""
+        if scene_Date:
+            newfilename+=re.sub('\s+$', '', scene_Date)
+        if performer_name:
+            newfilename+=" " + re.sub('\s+$', '', performer_name)
+        newfilename+=" - " + re.sub('\s+$', '', scene_Title)
+        if studio_name != "":
+            newfilename+=" [" + studio_name + "]"
+        newfilename+=scene_Extension
+
+        #Remove illegal character for Windows ('#' and ',' is not illegal you can remove it)
+        newfilename = re.sub('[\\/:"*?<>|#,]+', '', newfilename) 
+        newpath = scene_Directory + "\\" + newfilename
+        if (newpath == scene_fullPath):
+            #print("Files already renamed (Db).")
+            #print("\n")
+            continue
+        else:
             print("ID: ",scene_ID)
             print("Path: ",scene_fullPath)
             print("Directory:",scene_Directory)
             print("Extension: ",scene_Extension)
             print("Title: ",scene_Title)
             print("Date: ",scene_Date)
-
-                #  Look for duplicate title
-            
-            cursor.execute("SELECT path FROM scenes WHERE title='" + scene_Title.replace("'", "''") + "' AND NOT id='" + scene_ID + "';")
-            duplicateCheck = cursor.fetchall()
-            if (len(duplicateCheck) > 0):
-                problem=0
-                for dupl_row in duplicateCheck:
-                    if (os.path.dirname(str(dupl_row[0])) == scene_Directory):
-                        with open('output.txt', 'a') as f:
-                            f.write('Duplicated title detected!\n')
-                            f.write('{} - {}\n'.format(scene_ID, scene_Title))
-                            f.write('{} - {}\n'.format(os.path.dirname(str(dupl_row[0])), scene_Directory))
-                        problem=1
-                if (problem == 1):
-                        # Skip this file to avoid overwrite files
-                    print("\n")
-                    continue
-
-
-            performer_name=get_Perf_fromScene(scene_ID)
+            print("Studio ID: ",scene_Studio_id)
             print("Performer name: ",performer_name)
-
-
-            if (scene_Studio_id and scene_Studio_id != "None"):
-                studio_name=get_Studio_fromID(scene_Studio_id)
-                print("Studio name: ",studio_name)
-
-            # Title + Extension
-            newfilename = str(scene_Title + scene_Extension)
-
-            # Date + Performer + Title
-            #if not scene_Date: # Date is not set
-            #    newfilename = str(performer_name + "- " + scene_Title + scene_Extension)
-            #else:
-            #    if (performer_name.isspace() or not performer_name): # Don't have performer
-            #        newfilename = str(scene_Date  + " " + scene_Title + scene_Extension)
-            #    else:
-            #        newfilename = str(scene_Date + " " + performer_name + "- " + scene_Title + scene_Extension)
-
-            #################################
-            
-            newfilename = re.sub('[\\/:"*?<>|#,]+', '', newfilename) #Remove illegal character for Windows (# and , is not illegal you can remove it)
-            newpath = scene_Directory + "\\" + newfilename
+            print("Studio name: ",studio_name)
+            print("-------------")
+            print("OldFilename: ",os.path.basename(scene_fullPath)) # Get filename
             print("NewFilename: ",newfilename)
             print("NewPath: ",newpath)
-            if (newpath == scene_fullPath):
-                print("Files already renamed (Db).")
-                print("\n")
-                continue
-            else:
-                # THIS PART WILL EDIT YOUR DATABASE, FILES (be careful and know what you do)
 
-                # Windows Rename
-                if (os.path.isfile(scene_fullPath) == True):
-                    os.rename(scene_fullPath,newpath)
-                    if (os.path.isfile(newpath) == True):
-                        print("File Renamed!", newpath)
-                    else:
-                        print("Error, rename doesn't work ?",newpath, file=open("output.txt", "a"))
+            # 
+            # THIS PART WILL EDIT YOUR DATABASE, FILES (be careful and know what you do)
+            #
+            # Windows Rename
+            if (os.path.isfile(scene_fullPath) == True):
+                os.rename(scene_fullPath,newpath)
+                if (os.path.isfile(newpath) == True):
+                    print("File Renamed!", newpath)
                 else:
-                    print("File don't exist in Explorer")
+                    print("Error ?",newpath, file=open("output.txt", "a"))
+            else:
+                print("File don't exist in Explorer")
 
-                    # Database rename
-                cursor.execute("UPDATE scenes SET path='" + newpath + "' WHERE id=" + scene_ID + ";")
-                edit+=1
-                if (edit > 10):
-                    sqliteConnection.commit()
-                    print("[Database] Update database!")
-                    edit=0
+            # Database rename
+            cursor.execute("UPDATE scenes SET path='" + newpath + "' WHERE id=" + scene_ID + ";")
+            edit+=1
+            # I update the database every 10 files, you can change this number.
+            if (edit > 10):
+                sqliteConnection.commit()
+                print("[Database] Datebase Updated!")
+                edit=0
 
-                # Rclone rename
-                #rclone_CURRENT=scene_fullPath.replace('H:','gdcrypt:').replace("\\","/")
-                #rclone_NEW=newpath.replace('H:','gdcrypt:').replace("\\","/")
-                #rclone_Process = subprocess.Popen(['rclone', 'moveto', rclone_CURRENT, rclone_NEW, '-v', '--log-file=C:\\Users\\Winter\\Documents\\RcloneLog.log'],shell=True)
-                #rclone_Process.communicate()
-                #print("\n")
-
+            # Rclone rename
+            #rclone_CURRENT=scene_fullPath.replace('H:','gdcrypt:').replace("\\","/")
+            #rclone_NEW=newpath.replace('H:','gdcrypt:').replace("\\","/")
+            #rclone_Process = subprocess.Popen(['rclone', 'moveto', rclone_CURRENT, rclone_NEW, '-v', '--log-file=C:\\Users\\Winter\\Documents\\RcloneLog.log'],shell=True)
+            #rclone_Process.communicate()
+            #print("\n")
+            print("\n")
+        progress.finish()
     cursor.close()
 except sqlite3.Error as error:
-    print("Error while connecting to sqlite", error)
+    print("SQLITE Error: ", error)
 finally:
     if (sqliteConnection):
         sqliteConnection.commit()
         sqliteConnection.close()
         print("The SQLite connection is closed")
+# Input if you want to check the console.
+input("Press Enter to continue...")
